b0a989e5b65194ff68866ecaea251aab900bbc4e
laylson <laylson01@hotmail.com>
2021-06-17 22:25:42 -0300

    Game

---
+++ forca.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+
+    //cout << "hello world" << endl;
+
+    string palavra_secret = "MELANCIA";
+    cout << palavra_secret << endl;
+
+}
