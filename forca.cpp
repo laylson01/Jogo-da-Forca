00ce671e7494df4cecab8ed9e72323da2bbc99fe
laylson <laylson01@hotmail.com>
2021-06-17 23:26:32 -0300

    definindo palavra, chute d/letra e funçao para verificar letra

--- forca.cpp
+++ forca.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main () {
 
+bool letra_existe(char chute){
+
+} 
+
+// M E L A N C I A
+//0 1 0 1 0 1 0 1
+int main()
+{
 
     //cout << "hello world" << endl;
-
+    // char palavra_secret = [20]; Old way
     string palavra_secret = "MELANCIA";
     cout << palavra_secret << endl;
 
+    bool n_acertou = true;
+    bool n_enforcou = true;
+
+    while (n_acertou & n_enforcou)
+    {
+        char chute;
+        cin >> chute;
+        cout << "Seu chute foi " << chute << endl;
+
+        while (n_acertou && n_enforcou)
+        {
+            char chute;
+            cin >> chute;
+
+            if (letra_existe(chute))
+            {
+                cout << "Você acertou! Seu chute está na palavra." << endl;
+            }
+            else
+            {
+                cout << "Você errou! Seu chute não está na palavra." << endl;
+            }
+        }
+    }
 }
