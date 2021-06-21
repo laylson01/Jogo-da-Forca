9570cbd26702989fa53947ed5b013fbd97ec8f63
laylson <laylson01@hotmail.com>
2021-06-21 19:08:06 -0300

    FOR (C++ para C++11)

--- forca.cpp
+++ forca.cpp
@@ -6,23 +6,38 @@ const string PALAVRA_SECRET = "MELANCIA";
 
 bool letra_existe(char chute)
 {
-    for (int i = 0; i < PALAVRA_SECRET.size(); i++)
+    for (char letra : PALAVRA_SECRET)
+    {
+        if (chute == letra)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/* for (int i = 0; i < PALAVRA_SECRET.size(); i++)
         if (chute == PALAVRA_SECRET[i])
         {
             return true;
         }
+
     return false;
-}
+*/
 
 /* M E L A N C I A
 0 1 0 1 0 1 0 1
 
-
-PALAVRA_SECRET
-
-
-
+PALAVRA_SECRET [0] = M
+PALAVRA_SECRET [1] = E
+PALAVRA_SECRET [2] = L
+PALAVRA_SECRET [3] = A
+PALAVRA_SECRET [4] = N
+PALAVRA_SECRET [5] = C
+PALAVRA_SECRET [6] = I
+PALAVRA_SECRET [7] = A
 */
+
 int main()
 {
 
