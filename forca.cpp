6d38d263c90d4a7fb014c994f579fbcaa4a069ea
laylson <laylson01@hotmail.com>
2021-06-21 22:19:22 -0300

    Corrigindo o bug do 2x while e escrevendo a palavra secreta

--- forca.cpp
+++ forca.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
-const string PALAVRA_SECRET = "MELANCIA";
+const string PALAVRA_SECRET = "GATO";
+map<char, bool> chutou;
 
 bool letra_existe(char chute)
 {
@@ -24,49 +26,47 @@ bool letra_existe(char chute)
 
     return false;
 */
-
-/* M E L A N C I A
-0 1 0 1 0 1 0 1
-
-PALAVRA_SECRET [0] = M
-PALAVRA_SECRET [1] = E
-PALAVRA_SECRET [2] = L
-PALAVRA_SECRET [3] = A
-PALAVRA_SECRET [4] = N
-PALAVRA_SECRET [5] = C
-PALAVRA_SECRET [6] = I
-PALAVRA_SECRET [7] = A
-*/
-
 int main()
 {
-
-    //cout << "hello world" << endl;
-    // char PALAVRA_SECRET = [20]; Old way
-    cout << PALAVRA_SECRET << endl;
-
+    // cout << PALAVRA_SECRET << endl;
     bool n_acertou = true;
     bool n_enforcou = true;
 
     while (n_acertou & n_enforcou)
     {
-        char chute;
-        cin >> chute;
-        cout << "Seu chute foi " << chute << endl;
-
-        while (n_acertou && n_enforcou)
+        for (char letra : PALAVRA_SECRET)
         {
-            char chute;
-            cin >> chute;
-
-            if (letra_existe(chute))
+            if (chutou[letra])
             {
-                cout << "Você acertou! Seu chute está na palavra." << endl;
+                cout << letra << " ";
             }
             else
             {
-                cout << "Você errou! Seu chute não está na palavra." << endl;
+                cout << "_ ";
             }
         }
+        cout << endl;
+        char chute;
+        cin >> chute;
+        chutou[chute] = true;
+
+        if (letra_existe(chute))
+        {
+            cout << "Você acertou! Seu chute está na palavra." << endl;
+        }
+        else
+        {
+            cout << "Você errou! Seu chute não está na palavra." << endl;
+        }
+
+        /* chutou["M"] = false;
+            chutou["E"] = false;
+            chutou["L"] = false;
+            chutou["A"] = false;
+            chutou["N"] = false;
+            chutou["C"] = false;
+            chutou["I"] = false;
+            chutou["A"] = false; 
+            */
     }
 }
